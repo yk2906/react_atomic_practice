aac0ba7db62a203646b3d8bd4abe8d2d504f0e15
yk2906 <56224546+yk2906@users.noreply.github.com>
2021-11-05 16:55:30 +0900

    Initial commit

---
+++ src/components/molecules/SearchInput.jsx
@@ -0,0 +1,10 @@
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+
+export const SearchInput = () => {
+  return (
+    <div>
+      <input />
+      <PrimaryButton>検索</PrimaryButton>
+    </div>
+  );
+};
